3b780ffd82ecd0184f13f047438f0b0b08d9f57f
Ramkumar96 <sivaram0203@gmail.com>
2020-04-14 23:51:37 +0530

    Route to order

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -7,24 +7,25 @@ import ContactData from '../Checkout/ContactData/ContactData';
 class Checkout extends Component {
 
     state ={
-        ingredients : {
-            salad :1,
-            meat:1,
-            cheese: 1,
-            bacon :1
+        ingredients : null,
+        price: 0
         }
-    }
 
-    componentDidMount() {
-        // console.log(this.props.location);
+    componentWillMount() {
+
         const query = new URLSearchParams(this.props.location.search);
-        
+
         const ingredients = {};
-        for( let param of query.entries()){
-            ingredients[param[0]] = +param[1];       
+        let price = 0;
+        for( let param of query.entries() ){
+  
+            if(param[0] === 'price'){
+                 price = param[1];
+            } else{
+                ingredients[param[0]] = +param[1];   
+            }        
         }
-        console.log(query.entries());
-        this.setState({ingredients: ingredients})  
+        this.setState({ingredients: ingredients ,totalPrice: price}); 
     }
 
     checkoutCancelledHandler = () => {
@@ -33,7 +34,7 @@ class Checkout extends Component {
 
     checkoutContinedHandler = () => {
         this.props.history.replace('/checkout/contact-data');
-    }
+    } 
  
     
     render() {
@@ -44,7 +45,9 @@ class Checkout extends Component {
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinedHandler} />
                 <Route path={this.props.match.path + '/contact-data'} 
-                        component={ContactData} />
+                         render={(props) =>(<ContactData ingredients={this.state.ingredients} 
+                          price = {this.state.totalPrice}
+                          {...props}/>)} />
             </div>
         );
     }
